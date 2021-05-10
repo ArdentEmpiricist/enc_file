61fcf092c24e0bb379a48d3caaebbe8fafaa6c56
Hendrik Lux <35100899+LazyEmpiricist@users.noreply.github.com>
2021-05-10 09:13:08 +0200

    Fixed typos

--- src/lib.rs
+++ src/lib.rs
@@ -415,7 +415,7 @@ pub fn get_sha256_hash(data: Vec<u8>) -> Result<String, Box<dyn std::error::Erro
     Ok(format!("{:?}", hash))
 }
 
-/// Get SHA512 Hash from data. File needs to be read as Vac<u8> (e.g. use enc_file::read_file()). Returns result.
+/// Get SHA512 Hash from data. File needs to be read as Vec<u8> (e.g. use enc_file::read_file()). Returns result.
 /// # Examples
 ///
 /// ```
@@ -425,7 +425,7 @@ pub fn get_sha256_hash(data: Vec<u8>) -> Result<String, Box<dyn std::error::Erro
 /// let test = b"Calculating the BLAKE3 Hash of this text".to_vec();
 /// let test2 = b"Calculating the BLAKE3 Hash of this different text".to_vec();
 ///
-/// //hashing 2x test and 1x text2 to compare the hashes. hash1 == hash2 != hash3
+/// //hashing 2x test and 1x test2 to compare the hashes. hash1 == hash2 != hash3
 /// let hash1 = get_sha512_hash(test.clone()).unwrap();
 /// let hash2 = get_sha512_hash(test).unwrap();
 /// let hash3 = get_sha512_hash(test2).unwrap();
